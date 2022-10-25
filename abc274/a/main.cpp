1425073216313541cd6c67c67ee2f22fdd025c31
Hietan <hietan.prog@gmail.com>
2022-10-25 14:06:34 +0900

    solve abc274/a

---
+++ abc274/a/main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0, i < (int)n; i++)
+
+using namespace std;
+
+int main(void){
+  int a, b;
+  cin >> a >> b;
+  
+  printf("%.3f\n", (double)b/a);
+
+  return 0;
+}
