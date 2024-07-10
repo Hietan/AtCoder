bf8e95e85b975c48495c12b3bf5e2bc1f25c2f39
Hietan <hietan.code@gmail.com>
2024-07-10 13:29:27 +0900

    fix bugs

--- Libraries/cpp/HietanLibrary.hpp
+++ Libraries/cpp/HietanLibrary.hpp
@@ -4,9 +4,9 @@ using namespace std;
 
 // rep macro
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
-#define rep_s(i, s, n) for (int i = s; i < n; i++)
-#define rep_e(i, e) for (int i = 0; i <= e; i++)
-#define rep_se(i, s, e) for (int i = s; i <= e; i++)
+#define rep_s(i, s, n) for (int i = (int)s; i < (int)n; i++)
+#define rep_e(i, e) for (int i = 0; i <= (int)e; i++)
+#define rep_se(i, s, e) for (int i = (int)s; i <= (int)e; i++)
 
 // print YES or NO
 #define YON(b) cout << (b ? "YES" : "NO") << endl;
@@ -34,16 +34,18 @@ ostream& operator<<(ostream& os, V<T> v) {
 template <typename T>
 class V2 : public V<V<T>> {
 	public:
+		V2(int i, int j): V<V<T>>(i, V<T>(j)) {}
 		V2(int i, int j, T x): V<V<T>>(i, V<T>(j, x)) {}
 };
 
 template <typename T>
-istream& operator>>(istream& is, V2<T> v) {
-	for (V r: v) for (T e: r) is >> e;
+istream& operator>>(istream& is, V2<T>& v) {
+	for (V<T>& r: v) for (T& e: r) is >> e;
 	return is;
 }
 
 template <typename T>
 ostream& operator<<(ostream& os, V2<T> v) {
-	for (V r: v) os << r << endl;
+	for (V<T> r: v) os << r;
+	return os;
 }
