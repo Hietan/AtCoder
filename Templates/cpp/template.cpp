7d16c92f87e1595a01f2a67577027fb0c610bec9
Hietan <hietan.code@gmail.com>
2024-07-10 12:08:12 +0900

    Add template

---
+++ Templates/cpp/template.cpp
@@ -0,0 +1,16 @@
+#ifdef INCLUDED_MAIN
+
+int main() {
+
+}
+
+# else
+
+#include <bits/stdc++.h>
+#include <HietanLibrary>
+using namespace std;
+
+#define INCLUDED_MAIN
+#include __FILE__
+
+#endif
